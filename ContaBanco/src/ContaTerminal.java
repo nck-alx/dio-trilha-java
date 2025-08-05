241a563eac76d702076d6d4f9f98c6338158a37a
Nicolas Alexandre <alexandrenicolas509@gmail.com>
2025-08-05 14:31:22 -0300

    adicionando os comentários

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -6,21 +6,21 @@ public class ContaTerminal {
         System.out.println("BEM VINDO AO BANCO WOODSTONE. \nPressione ENTER para criar sua conta");
         scanner.nextLine();
         System.out.print("Digite o código de sua agência: ");
-        var agencia = scanner.next();
+        var agencia = scanner.next(); //scanner identifica o número digitado e armazena na variável agencia
         
         System.out.print("Digite o número de sua conta: ");
-        var numero = scanner.nextInt();
+        var numero = scanner.nextInt(); //scanner identifica o número digitado e armazena na variável numero
         
         System.out.print("Digite seu primeiro e segundo nome: ");
-        scanner.nextLine();
-        var nome = scanner.nextLine();
+        scanner.nextLine(); //consome o "enter" que sobrou
+        var nome = scanner.nextLine(); //scanner identifica o que foi digitado e armazena na variável nome
 
         System.out.print("Insira o valor do seu saldo: ");
-        var saldo = scanner.nextDouble();
+        var saldo = scanner.nextDouble(); //scanner identifica o numero digitado e armazena na variável saldo
 
         System.out.printf("Olá, %s! Obrigado por criar uma conta no Banco Woodstone.%n", nome);
         System.out.printf("Agência: %s%nConta: %d%nSaldo: R$ %.2f%n", agencia, numero, saldo);
-        scanner.close();
+        scanner.close(); //fecha o scanner
 
     }
 }
