ca8c5daa9b301b526d63fee8f9ec9b5c55557cea
Nicolas Alexandre <alexandrenicolas509@gmail.com>
2025-08-05 14:27:00 -0300

    Terminal simples de criação de conta no banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        var scanner = new Scanner(System.in);
+        System.out.println("BEM VINDO AO BANCO WOODSTONE. \nPressione ENTER para criar sua conta");
+        scanner.nextLine();
+        System.out.print("Digite o código de sua agência: ");
+        var agencia = scanner.next();
+        
+        System.out.print("Digite o número de sua conta: ");
+        var numero = scanner.nextInt();
+        
+        System.out.print("Digite seu primeiro e segundo nome: ");
+        scanner.nextLine();
+        var nome = scanner.nextLine();
+
+        System.out.print("Insira o valor do seu saldo: ");
+        var saldo = scanner.nextDouble();
+
+        System.out.printf("Olá, %s! Obrigado por criar uma conta no Banco Woodstone.%n", nome);
+        System.out.printf("Agência: %s%nConta: %d%nSaldo: R$ %.2f%n", agencia, numero, saldo);
+        scanner.close();
+
     }
 }
